6c7177b41e4f1bb6e0ae8fc7412fde3cfbb4342e
Eric Gerzenstein <112182259+elmUffins@users.noreply.github.com>
2024-06-26 10:37:04 -0300

    Crear Script
    Con variables

---
+++ Assets/Scripts/CAIDA.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAIDA : MonoBehaviour
+{
+    public GameObject[] productos;
+    public int min;
+    public int max;
+    void Start()
+    {
+        int cantidad = Random.Range(4, 14);
+        int elemento = Random.Range(0, 20);
+    }
+
+    void Update()
+    {
+        
+    }
+}

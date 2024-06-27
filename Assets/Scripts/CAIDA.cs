b6dff8c482fe29757cbc5d270356d61ff4ed891f
elmUffins <ericyes81007@gmail.com>
2024-06-26 22:11:53 -0300

    Juntar todo + arreglos V1
    Finalizar e implementar código en sección con assets, colorear el plano, agrandarlo, poner una pared, arreglar ciertos prefabs.

--- Assets/Scripts/CAIDA.cs
+++ Assets/Scripts/CAIDA.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class CAIDA : MonoBehaviour
 {
-    public GameObject[] productosPrefab;
-    public Transform[] spawners;
-    int spawner = 0;
-    int cantidad = Random.Range(4, 20);
-    int elemento = Random.Range(0, 20);
+    public GameObject[] productosPrefab; // Todos los productos disponibles
+    public Transform[] spawners; 
+    public float delay = 1.0f; 
+
     void Start()
+    {
+        int cantidad = Random.Range(4, 20); 
+        int elemento = Random.Range(0, 20); 
+        StartCoroutine(SpawnObjects(cantidad, elemento));
+        Debug.Log(cantidad);
+    }
+
+    IEnumerator SpawnObjects(int cantidad, int elemento)
     {
         for (int i = 0; i < cantidad; i++)
         {
-            Instantiate(productosPrefab[elemento], spawners[spawner].position, spawners[spawner].rotation);
-            if (spawner + 1 == 9)
-            {
-                spawner = 0;
-            }
-            else
-            {
-                spawner += 1;
-            }
+            int spawnerIndex = Random.Range(0, spawners.Length); 
+            Instantiate(productosPrefab[elemento], spawners[spawnerIndex].position, spawners[spawnerIndex].rotation);
+            yield return new WaitForSeconds(delay); 
         }
-        
     }
 
     void Update()

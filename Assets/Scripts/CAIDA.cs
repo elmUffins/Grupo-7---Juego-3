18dfa8b09164fccc000c1fe9a5419c314ef0d5dd
elmUffins <ericyes81007@gmail.com>
2024-06-26 22:28:32 -0300

    Juntar todo + arreglos v1.1
    Borrar lo innecesario, cambiar nombres a cosas más relevantes, permitir cambio de pantalla.

--- Assets/Scripts/CAIDA.cs
+++ Assets/Scripts/CAIDA.cs
@@ -1,22 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CAIDA : MonoBehaviour
 {
-    public GameObject[] productosPrefab; // Todos los productos disponibles
-    public Transform[] spawners; 
-    public float delay = 1.0f; 
+    public GameObject[] productosPrefab; // Array de todos los productos disponibles
+    public Transform[] spawners; // Array con los spawners
+    public float delay = 1.0f; // Delay entre cada instanciación del prefab
+    public float delayDeCambio = 5.0f; // Delay entre la caída del último elemento y el cambio de pantalla
 
     void Start()
     {
         int cantidad = Random.Range(4, 20); 
         int elemento = Random.Range(0, 20); 
-        StartCoroutine(SpawnObjects(cantidad, elemento));
+        StartCoroutine(SpawnObjects(cantidad, elemento)); // Activa el proceso 'SpawnObjects'
         Debug.Log(cantidad);
     }
 
-    IEnumerator SpawnObjects(int cantidad, int elemento)
+    IEnumerator SpawnObjects(int cantidad, int elemento) // Función de tipo 'IEnumerator', permite iterar los objetos y utilizar 'WaitForSeconds'
     {
         for (int i = 0; i < cantidad; i++)
         {
@@ -24,6 +26,10 @@ public class CAIDA : MonoBehaviour
             Instantiate(productosPrefab[elemento], spawners[spawnerIndex].position, spawners[spawnerIndex].rotation);
             yield return new WaitForSeconds(delay); 
         }
+
+        // Cambio de escena
+        yield return new WaitForSeconds(delayDeCambio);
+        SceneManager.LoadScene("RESPUESTA"); 
     }
 
     void Update()

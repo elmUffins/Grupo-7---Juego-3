edc565dbe9047e08bbb4f877d669c288f5c1c2f7
BorjaIzu <borja.izurieta@gmail.com>
2024-06-27 09:07:04 -0300

    cambios script
    aparicion y desaparicion de botones, textos y paneles script botones

--- Assets/Scripts/BOTONES.cs
+++ Assets/Scripts/BOTONES.cs
@@ -8,10 +8,11 @@ public class BOTONES : MonoBehaviour
     public InputField inputField; 
     public GameObject panelNumero; 
     public GameObject panelError;
-
+    public GameObject esperando;
 
     void Start()
     {
+        esperando.SetActive(true);
         panelNumero.SetActive(false);
         panelError.SetActive(false);
     }
@@ -22,11 +23,13 @@ public class BOTONES : MonoBehaviour
 
         if (int.TryParse(inputText, out int result))
         {
+            esperando.SetActive(false);
             panelNumero.SetActive(true);
             panelError.SetActive(false);
         }
         else
         {
+            esperando.SetActive(false);
             panelNumero.SetActive(false);
             panelError.SetActive(true);
         }

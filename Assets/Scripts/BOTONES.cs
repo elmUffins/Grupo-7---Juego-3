a668e32901b4acccf69e64459f05e1938e54471f
elmUffins <ericyes81007@gmail.com>
2024-06-26 22:58:11 -0300

    Crear script botones
    Agregar cierta funcionalidad a los botones

---
+++ Assets/Scripts/BOTONES.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BOTONES : MonoBehaviour
+{
+    public InputField inputField; 
+    public GameObject panelNumero; 
+    public GameObject panelError;
+
+
+    void Start()
+    {
+        panelNumero.SetActive(false);
+        panelError.SetActive(false);
+    }
+
+    public void Mostrar()
+    {
+        string inputText = inputField.text;
+
+        if (int.TryParse(inputText, out int result))
+        {
+            panelNumero.SetActive(true);
+            panelError.SetActive(false);
+        }
+        else
+        {
+            panelNumero.SetActive(false);
+            panelError.SetActive(true);
+        }
+    }
+}
